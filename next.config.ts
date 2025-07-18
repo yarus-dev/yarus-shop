fc0fe39874fa7678b06e0e83f44e525024559099
Yaroslav Usenko <152315680+yarus-dev@users.noreply.github.com>
2025-07-19 01:57:55 +0300

    refactor: remove Firebase client and server code
    - Deleted Firebase client initialization and configuration files.
    - Removed server-side Firebase authentication logic.
    - Updated package.json to remove Firebase dependency and adjust Next.js version.

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
   poweredByHeader: false,
-  pageExtensions: ["ts", "tsx", "md", "mdx"],
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+  pageExtensions: ["ts", "tsx", "md", "mdx"]
 };
 
 export default nextConfig;
