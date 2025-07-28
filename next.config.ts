46ff9ba5c69c937e4a4e227f6b1ebe801325ff27
Yaroslav Usenko <152315680+yarus-dev@users.noreply.github.com>
2025-07-29 01:53:51 +0300

    style: standardize quotation marks in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   poweredByHeader: false,
-  pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+  pageExtensions: ["ts", "tsx", "md", "mdx"],
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
