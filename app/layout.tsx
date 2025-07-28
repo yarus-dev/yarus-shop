9f9d2f56017e05c64b5c069c31d07a5d6903de3a
Yaroslav Usenko <152315680+yarus-dev@users.noreply.github.com>
2025-07-29 01:44:51 +0300

    feat: update analytics import to use Firebase for tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "@/app/globals.css"
-import "@/lib/analytics"
 import { cn } from "@/lib/cn"
+import "@/lib/firebase/analytics"
 import { getOriginUrl } from "@/lib/utils"
 import type { Metadata } from "next"
 import { Rubik } from "next/font/google"
